678f29767f9e8f45d47134e2eaa8604f971bef75
md sahadat hossin <mdsahadathossin778@gmail.com>
2022-06-02 16:46:05 +0600

    fix height issue

--- components/home/hero/hero.js
+++ components/home/hero/hero.js
@@ -2,7 +2,11 @@ import styles from "./hero.module.scss";
 const Hero = () => {
   return (
     <div className={styles.main}>
-      <h1 className={styles.title}>Wellcome to SSR GEEK </h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iste
+      neque ipsum hic, temporibus dolores consequatur assumenda quaerat impedit
+      recusandae repellat quidem iure excepturi aliquam laboriosam? Temporibus
+      officia debitis ut?
+      {/* <h1 className={styles.title}>Wellcome to SSR GEEK </h1>
       <p className={styles.subTitle}>we build React , Nextjs Applications</p>
       <div className={styles.logos}>
         <img
@@ -26,7 +30,7 @@ const Hero = () => {
           src="node.svg"
           alt="nodejs"
         />
-      </div>
+      </div> */}
     </div>
   );
 };
