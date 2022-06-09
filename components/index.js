f71d89634d0751f3be9281741d5d3bc2129434ba
md sahadat hossin <mdsahadathossin778@gmail.com>
2022-06-09 12:51:54 +0600

    fixed

--- components/index.js
+++ components/index.js
@@ -2,7 +2,8 @@ export { default as Navbar } from "./home/Navbar/Navbar";
 export { default as Hero } from "./home/hero/hero";
 
 export { default as Layout } from "./Layout";
-export { default as OurServices } from "./home/OurServices/OurServices";
+
 export { default as RecentProjects } from "./home/RecentProjects/RecentProjects";
 export { default as TechnologiesUsed } from "./home/TechnologiesUsed/TechnologiesUsed";
 export { default as Testimonials } from "./home/Testimonials/Testimonials";
+export { default as OurServices } from "./home/OurServices/OurServices";
